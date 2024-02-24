b981b1cc6022726775033c56f0d252fc9d9b0998
ganesh padole <ganeshpadole55@gmail.com>
2024-02-24 18:43:17 +0530

    - added custom button and textfield

---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,34 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Ganesh Padole on 24/02/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
